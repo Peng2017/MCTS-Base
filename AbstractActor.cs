25d8280fe87807fa37f256a3efa6e1809c5c6028
Penndrageist <penndrageist.dev@gmail.com>
2017-05-25 13:04:05 +1000

    Update AbstractActor.cs

--- AbstractActor.cs
+++ AbstractActor.cs
@@ -7,5 +7,21 @@ namespace MCTS
      */
     public abstract class AbstractActor
     {
+        public String identifier
+        {
+            get;
+        }
+        
+        public bool Equals(Object other)
+        {
+            if(other is AbstractActor)
+            {
+                AbstractActor aa = (AbstractActor) other;
+                
+                return aa.identifier.Equals(this.identifier);
+            }
+            else
+                return false;
+        }
     }
 }
